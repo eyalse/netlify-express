3e1ea4de834235dea56e1c2e0de7205d2a2c24c3
Eyal Segal <eyal.segal@wework.com>
2019-06-01 19:46:38 +0300

    init

--- express/server.js
+++ express/server.js
@@ -1,20 +1,52 @@
 'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
+const uuidv4 = require('uuid/v4')
 const bodyParser = require('body-parser');
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+let items = [
+  { id: uuidv4(), item: 'Teach me about PWA' },
+  { id: uuidv4(), item: 'Make an awesome app' }
+]
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router
+  .get('/items.json', (req, res) => {
+    res.json(items)
+  })
+  .post('/items.json', (req, res) => {
+    items.push({
+      id: uuidv4(),
+      item: req.body.item
+    })
+    res.json(items)
+  })
+  .delete('/items.json', (req, res) => {
+    items = items.filter(item => {
+      if(item.id !== req.body.id) {
+        return item
+      }
+    })
+    res.json(items)
+  })
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-
+  app.use('/.netlify/functions/server', router);  // path must route to lambda
 module.exports = app;
 module.exports.handler = serverless(app);
